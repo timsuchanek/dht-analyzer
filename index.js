4f3740aabee5cf0efb9e40e34c05985c95c62a9a
timsuchanek <tim.suchanek@gmail.com>
2014-06-04 23:49:37 +0200

    refactor

--- index.js
+++ index.js
@@ -4,38 +4,42 @@ var lines = fs.readFileSync('CountryTrendAnalyzer.txt')
 .toString()
 .split('\n');
 
-var commentRegex = /#+(.*)/;
-// only use lines, which are no comments
-lines = lines.filter(function(line) {
-  return !commentRegex.test(line);
-});
+function processData(lines) {
+  var commentRegex = /#+(.*)/;
+  // only use lines, which are no comments
+  lines = lines.filter(function(line) {
+    return !commentRegex.test(line);
+  });
 
-var lineRegex = /(\d+)\s\|\s[(((\w{2})\s\|\s(0\.\d*),*)+)]/;
+  var muchEasierLineRegex = /(\d+)\s|\s[(.*)]/;
 
-var muchEasierLineRegex = /(\d+)\s|\s[(.*)]/;
+  var information = [];
+  lines.forEach(function(line) {
+    var splitted = line.split(muchEasierLineRegex);
+    var timestamp = parseInt(splitted[1], 10);
+    var rest = splitted[2];
+    if (rest) {
+      rest = rest.slice(3, rest.length - 1);
+      var countries = rest.split(', ');
+      countries = countries.map(function(country) {
+        var country = country.split(' | ');
+        country[1] = parseFloat(country[1], 10);
+        return country;
+      });
+      information.push({
+        timestamp: timestamp,
+        countries: countries
+      });
+    }
+  });
+  return information
+}
 
-var information = [];
-lines.forEach(function(line) {
-  var splitted = line.split(muchEasierLineRegex);
-  var timestamp = parseInt(splitted[1], 10);
-  var rest = splitted[2];
-  if (rest) {
-    rest = rest.slice(3, rest.length - 1);
-    var countries = rest.split(', ');
-    countries = countries.map(function(country) {
-      var country = country.split(' | ');
-      country[1] = parseFloat(country[1], 10);
-      return country;
-    });
-    information.push({
-      timestamp: timestamp,
-      countries: countries
-    });
-  }
-});
+var information = processData(lines);
 
 var lengthts = information.map(function(info) {
   return info.countries.length;
 });
+
 console.log(lengthts, lengthts.length);
 // console.log(information[0]);
