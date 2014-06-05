0eb4969cfc0fcccf9f8bc4833485df938eb817a6
timsuchanek <tim.suchanek@gmail.com>
2014-06-05 11:34:54 +0200

    Added values from bitcoinstatus

--- index.js
+++ index.js
@@ -81,23 +81,92 @@ function getCountryForCode(code) {
   return country;
 }
 
-var lines = fs.readFileSync('CountryTrendAnalyzer.txt')
-.toString()
-.split('\n');
-
-var information = processData(lines);
-
-var average = calcAverageValues(information);
-average.forEach(function(avg) {
-  var country = getCountryForCode(avg.country);
-  if (country) {
-    // avg.code = avg.country;
-    avg.country = country.name;
+function getPearsonsCorrelation(x, y) {
+  var shortestArrayLength = 0;
+  if (x.length == y.length) {
+    shortestArrayLength = x.length;
+  } else if (x.length > y.length) {
+    shortestArrayLength = y.length;
+    console.error('x has more items in it, the last ' + (x.length - shortestArrayLength) + ' item(s) will be ignored');
+  } else {
+    shortestArrayLength = x.length;
+    console.error('y has more items in it, the last ' + (y.length - shortestArrayLength) + ' item(s) will be ignored');
   }
-});
-average = average.sort(function(a,b) {
-  return b.dhtValue - a.dhtValue;
-});
-var averageString = JSON.stringify(average);
-fs.writeFileSync('average-dht-peers.json', averageString);
-console.log(average);
+
+  var xy = [];
+  var x2 = [];
+  var y2 = [];
+
+  for (var i = 0; i < shortestArrayLength; i++) {
+    xy.push(x[i] * y[i]);
+    x2.push(x[i] * x[i]);
+    y2.push(y[i] * y[i]);
+  }
+
+  var sum_x = 0;
+  var sum_y = 0;
+  var sum_xy = 0;
+  var sum_x2 = 0;
+  var sum_y2 = 0;
+
+  for (var i = 0; i < shortestArrayLength; i++) {
+    sum_x += x[i];
+    sum_y += y[i];
+    sum_xy += xy[i];
+    sum_x2 += x2[i];
+    sum_y2 += y2[i];
+  }
+
+  var step1 = (shortestArrayLength * sum_xy) - (sum_x * sum_y);
+  var step2 = (shortestArrayLength * sum_x2) - (sum_x * sum_x);
+  var step3 = (shortestArrayLength * sum_y2) - (sum_y * sum_y);
+  var step4 = Math.sqrt(step2 * step3);
+  var answer = step1 / step4;
+
+  return answer;
+}
+
+// var lines = fs.readFileSync('CountryTrendAnalyzer.txt')
+//   .toString()
+//   .split('\n');
+
+// var information = processData(lines);
+
+// var average = calcAverageValues(information);
+// average.forEach(function(avg) {
+//   var country = getCountryForCode(avg.country);
+//   if (country) {
+//     // avg.code = avg.country;
+//     avg.country = country.name;
+//   }
+// });
+// average = average.sort(function(a, b) {
+//   return b.dhtValue - a.dhtValue;
+// });
+// var averageString = JSON.stringify(average);
+// fs.writeFileSync('average-dht-peers.json', averageString);
+// console.log(average);
+
+var editedData = fs.readFileSync('average-dht-peers-edited.json');
+try {
+  editedData = JSON.parse(editedData);
+} catch (e) {
+  editedData = null;
+}
+
+if (editedData) {
+  var dhtValues = []
+    , barcelonaValues = []
+    , bitcoinStatusValues = [];
+  editedData.forEach(function(date) {
+    dhtValues.push(date.dhtValue);
+    barcelonaValues.push(date.bitcoinBarcelonaValue);
+    bitcoinStatusValues.push(date.bitcoinStatusValue);
+  });
+
+  var barcelonaCorrelation = getPearsonsCorrelation(dhtValues, barcelonaValues);
+  var bitcoinStatusCorrelation = getPearsonsCorrelation(dhtValues, bitcoinStatusValues);
+
+  console.log('Barca correlation: ', barcelonaCorrelation);
+  console.log('Bitcoin correlation: ', bitcoinStatusCorrelation);
+}
